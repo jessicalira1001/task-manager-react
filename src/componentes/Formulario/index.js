9bf5d4808ab32d3f2e4cfdb0e72424bd3b80545a
jessicalira1001 <jessicalira1001@gmail.com>
2025-04-09 20:49:54 -0300

    Feat: Adicionando botão Criar tarefa no formulário

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,18 +1,23 @@
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import './Formulario.css'
+import Botao from "../Botao";
 
 const dias = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo' ]
 
+const aoSalvar = (evento) => {
+    evento.preventDefault()
+    console.log("form foi submetido")
+}
 
 const Formulario = () => {
     return(
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
             <h2>Preencha as informações para adicionar uma tarefa</h2>
                 <CampoTexto  obrigatorio={true} label ="Tarefa" placeholder = "Digite a tarefa"></CampoTexto>
                 <ListaSuspensa obrigatorio={true} label='Dia' itens={dias}></ListaSuspensa>
-
+                <Botao>Criar tarefa</Botao>
             </form>
         </section>
     )
